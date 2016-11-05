e41f7abc304067503b0aca25a63b17876032dca1
tail12 <tdallb12@gmail.com>
2016-11-05 15:57:52 +0900

    login/logout func

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
   root 'home#top'
+
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  resources :users
 end
